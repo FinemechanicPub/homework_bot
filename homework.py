ba4645b75a69bc37c880a0567df2170fcddb521c
Alexander Rubtsov <equester@mail.ru>
2022-02-13 17:46:25 +0300

    Refactoring

--- homework.py
+++ homework.py
@@ -3,44 +3,43 @@
 import logging
 import os
 import sys
-import requests
 import time
 
+import requests
 from dotenv import load_dotenv
 from telegram import Bot
 from telegram.error import TelegramError
-from json.decoder import JSONDecodeError
 
 SUCCESS = 'В Telegram отправлено сообщение: "{message}"'
 FAILURE = 'Сбой в работе программы. {error_text}'
 REQUEST_FAIL = (
     'Не удалось подключиться к серверу. '
-    'Параметры запроса: url={url}, headers={headers}, params={params}.'
+    'Параметры запроса: url={url}, headers={headers}, params={params}. '
+    'Ошибка: {error_text}'
 )
 WRONG_STATUS_CODE = (
     'Сервер вернул неожиданный статус ответа: {status_code}. '
     'Параметры запроса: url={url}, headers={headers}, params={params}.'
 )
-WRONG_JSON = (
-    'Ответ сервера не является корректным json-объектом. '
-    'Параметры запроса: url={url}, headers={headers}, params={params}. '
-    'Текст ответа: {text}'
+SERVER_FAIL = (
+    'Сервер вернул сообщение "{name}" следующего содержания: {text} '
+    'Параметры запроса: url={url}, headers={headers}, params={params}.'
 )
 WRONG_RESPONSE_OBJECT = 'Вместо словаря от сервера получен объект {type_name}'
 WRONG_HOMEWORKS_OBJECT = (
     'Вместо списка домашних работ получен объект {type_name}'
 )
+NO_HOMEWORKS_IN_RESPONSE = 'В ответе сервера не обнаружено ключа homeworks.'
 WRONG_HOMEWORK_STATUS = 'Неизвестный статус домашней работы - "{status}"'
 STATUS_CHANGED = 'Изменился статус проверки работы "{name}". {verdict}'
-MISSING_VARIABLE = 'Не задана переменная среды {name}'
-TELEGRAM_ERROR = 'Ошибка при отправке сообщения в Telegram'
-ERROR_REPORT_FAIL = 'Ошибка при отправке сообщения об ошибке'
-CHECK_COMPLETE = 'Статус домашних работ проверен'
-NO_HOMEWORKS = 'Обновлений не получено'
+MISSING_VARIABLE = 'Не задана(ы) переменная(ые) среды: {names}'
+ERROR_REPORT_FAIL = 'Ошибка при отправке сообщения об ошибке: {error_text}'
+PROCESSING_COMPLETE = 'Обработка статуса домашних работ завершена успешно.'
+NO_HOMEWORK_UPDATE = 'Обновлений не получено.'
 
 error_cache = set()
 formatter = logging.Formatter(
-    '%(asctime)s [%(levelname)s] function "%(funcName)s" '
+    '%(asctime)s [%(levelname)s] Log "%(name)s" function "%(funcName)s" '
     'line %(lineno)d - %(message)s'
 )
 logger = logging.getLogger(__name__)
@@ -53,10 +52,10 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 load_dotenv()
-
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+VARIABLES = ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID = (
+    os.getenv(name) for name in VARIABLES
+)
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -77,28 +76,37 @@ def send_message(bot: Bot, message: str) -> None:
 
 def get_api_answer(current_timestamp: int) -> dict:
     """Запрос данных у сервера."""
+    request_data = {
+        'url': ENDPOINT,
+        'headers': HEADERS,
+        'params': {'from_date': current_timestamp}
+    }
+    response = None
     try:
-        request_data = {
-            'url': ENDPOINT,
-            'headers': HEADERS,
-            'params': {'from_date': current_timestamp}
-        }
         response = requests.get(**request_data)
-        if response.status_code != 200:
-            raise ValueError(WRONG_STATUS_CODE.format(
-                status_code=response.status_code, **request_data
+    except requests.RequestException as error:
+        raise ConnectionError(REQUEST_FAIL.format(
+            error_text=error, **request_data
+        ))
+    if response.status_code != 200:
+        raise ValueError(WRONG_STATUS_CODE.format(
+            status_code=response.status_code, **request_data
+        ))
+    json = response.json()
+    for key in ('error', 'code'):
+        if key in json:
+            raise ValueError(SERVER_FAIL.format(
+                name=key, text=json[key], **request_data
             ))
-        return response.json()
-    except JSONDecodeError:
-        raise ValueError(WRONG_JSON.format(text=response.text, **request_data))
-    except requests.RequestException:
-        raise ConnectionError(REQUEST_FAIL.format(**request_data))
+    return json
 
 
 def check_response(response: dict) -> list:
     """Извлечение списка домашних работ из ответа сервера."""
     if not isinstance(response, dict):
         raise TypeError(WRONG_RESPONSE_OBJECT.format(type_name=type(response)))
+    if 'homeworks' not in response:
+        raise KeyError(NO_HOMEWORKS_IN_RESPONSE)
     homeworks = response['homeworks']
     if not isinstance(homeworks, list):
         raise TypeError(
@@ -119,19 +127,12 @@ def parse_status(homework: dict) -> str:
 
 def check_tokens() -> bool:
     """Проверка наличия токенов доступа к сервисам."""
-    result = True
-    for var_name in ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID']:
-        # Проверяется считывание переменных среды в константы
-        # Наличие констант в словаре globals() предполагается
-        if not globals()[var_name]:
-            logger.critical(MISSING_VARIABLE.format(name=var_name))
-            result = False
-    return result
-
-
-def log_error(message: str):
-    """Запись сообщения об ошибке в лог."""
-    logger.exception(message)
+    missing_variables = [name for name in VARIABLES if not globals().get(name)]
+    if missing_variables:
+        logger.critical(
+            MISSING_VARIABLE.format(names=', '.join(missing_variables))
+        )
+    return not missing_variables
 
 
 def send_error(bot: Bot, message: str):
@@ -140,22 +141,14 @@ def send_error(bot: Bot, message: str):
         if message not in error_cache:
             send_message(bot, message)
             error_cache.add(message)
-    except Exception:
-        log_error(ERROR_REPORT_FAIL)
-
-
-def report_error(bot: Bot, error: Exception):
-    """Обработка ошибок."""
-    message = FAILURE.format(error_text=error)
-    log_error(message)
-    if not isinstance(error, TelegramError):
-        send_error(bot, message)
+    except Exception as error:
+        logger.exception(ERROR_REPORT_FAIL.format(error_text=error))
 
 
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
-        sys.exit(1)
+        raise SystemExit()
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     while True:
@@ -165,13 +158,18 @@ def main():
             if homeworks:
                 send_message(bot, parse_status(homeworks[0]))
             else:
-                logger.debug(NO_HOMEWORKS)
+                logger.debug(NO_HOMEWORK_UPDATE)
             current_timestamp = response.get('current_date', current_timestamp)
+        except TelegramError as error:
+            message = FAILURE.format(error_text=error)
+            logger.exception(message)
         except Exception as error:
-            report_error(bot, error)
+            message = FAILURE.format(error_text=error)
+            logger.exception(message)
+            send_error(bot, message)
         else:
             error_cache.clear()
-            logger.info(CHECK_COMPLETE)
+            logger.info(PROCESSING_COMPLETE)
         finally:
             time.sleep(RETRY_TIME)
 
